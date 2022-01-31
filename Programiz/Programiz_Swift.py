fa686df72089d6059036265807c5a2128ef4fdd0
mehuly25 <mehuly25@gmail.com>
2022-02-01 00:48:38 +0530

    Added files for Programiz, Geeks and W3School

---
+++ Programiz/Programiz_Swift.py
@@ -0,0 +1,175 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+mainURL = "https://www.programiz.com/swift-programming"
+
+page = requests.get(mainURL)
+htmlContent = page.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+
+urls = []
+pages = soup.find('div', {'class': 'right-bar'}).findAll(href=True)
+for slug in pages[:-7]:
+    urls.append('https://www.programiz.com' + slug.get('href'))
+
+mainHeadings = []
+mainContents = []
+videoHeadings = []
+videoURLs = []
+subHeadingsH2 = []
+subHeadingsH3 = []
+imageURLs = []
+facts = []
+definitions = []
+explanations = []
+codes = []
+snippets = []
+outputs = []
+tableData = []
+listItems = []
+data = {}
+
+for url in urls:
+    page = requests.get(url)
+    htmlContent = page.content
+    soup = BeautifulSoup(htmlContent, 'html.parser')
+
+    mainHeading = soup.findAll('h1')  # Main Heading
+    pageMainHeadings = []
+    if mainHeading is not None:
+        for head in mainHeading:
+            pageMainHeadings.append(head.text)
+
+    mainContent = soup.findAll('p', {"class": "editor-contents__short-description"})  # Main Content
+    pageMainContents = []
+    if mainContent is not None:
+        for mainCon in mainContent:
+            pageMainContents.append(mainCon.text)
+
+    videoHeading = soup.findAll('div', {"class": "programiz-youtube-wrapper lite-youtube"})  # Video Heading
+    pageVideoHeadings = []
+    if videoHeading is not None:
+        for vidHead in videoHeading:
+            pageVideoHeadings.append(vidHead.find('h2').text)
+
+    videoURL = soup.findAll('div', {"class": "programiz-youtube__container"})  # Video URL
+    pageVideoURLs = []
+    if videoURL is not None:
+        for vid in videoURL:
+            pageVideoURLs.append(vid.find('iframe').get('src'))
+
+    subContent = soup.find('div', {"class": "content"})  # Sub Content
+    if subContent is None:
+        continue
+
+    subHeadingH2 = subContent.findAll('h2')  # Sub Headings h2
+    pageSubHeadingsH2 = []
+    if subHeadingH2 is not None:
+        for head in subHeadingH2:
+            pageSubHeadingsH2.append(head.text)
+
+    subHeadingH3 = subContent.findAll('h3')  # Sub Headings h3
+    pageSubHeadingsH3 = []
+    if subHeadingH3 is not None:
+        for head in subHeadingH3:
+            pageSubHeadingsH3.append(head.text)
+
+    images = subContent.findAll('img')  # Images
+    pageImageURLs = []
+    if images is not None:
+        for img in images:
+            pageImageURLs.append(img.get('src'))
+
+    paragraphs = subContent.findAll('p')  # Paragraphs
+    pageFacts = []
+    pageDefinitions = []
+    pageExplanations = []
+    if paragraphs is not None:
+        for para in paragraphs:
+            slug = para.text
+            if 5 <= len(slug) <= 65:
+                pageFacts.append(slug)
+            elif 66 <= len(slug) <= 126:
+                pageDefinitions.append(slug)
+            elif 127 < len(slug):
+                pageExplanations.append(slug)
+
+    codeContent = subContent.findAll('pre')  # Codes, Snippets and Outputs
+    pageCodes = []
+    pageSnippets = []
+    pageOutputs = []
+    for slug in codeContent:
+        if slug.code is not None:
+            for cod in slug.code.text.split('\n'):
+                if cod != '' and not cod.lstrip().rstrip().startswith('//'):
+                    if 7 < len(cod) <= 30:
+                        pageSnippets.append(cod)
+                    elif 31 < len(cod):
+                        pageCodes.append(cod)
+
+        else:
+            for out in slug.text.split('\n'):
+                if out != '' and not out.lstrip().rstrip().startswith('//'):
+                    pageOutputs.append(out)
+                else:
+                    continue
+
+    tables = subContent.findAll('table')  # Tables
+    pageTableData = []
+    if tables is not None:
+        for tb in tables:
+            table = ""
+            rowData = []
+            tableRows = tb.findAll('tr')
+            for row in tableRows:
+                rowData.append(row.text[1:-1].replace('\n', '|||'))
+            for row in rowData:
+                table = table + str(row) + '&&&'
+            pageTableData.append(table[:-3])
+
+    allListUL = subContent.findAll('ul')  # List Items
+    allListOL = subContent.findAll('ol')
+    listUL = ""
+    listOL = ""
+    listItem = ""
+    pageListItems = []
+    if len(allListUL) != 0 or len(allListOL) != 0:
+        if len(allListUL) != 0:
+            for lis in allListUL:
+                listUL = listUL + str(lis.text).replace('\n', '|||').replace('\t\t', '')
+        if len(allListOL) != 0:
+            for lis in allListOL:
+                listOL = listOL + str(lis.text).replace('\n', '|||').replace('\t\t', '')
+
+        if len(allListUL) != 0 and len(allListOL) == 0:
+            listItem = listItem + listUL
+        elif len(allListUL) == 0 and len(allListOL) != 0:
+            listItem = listItem + listOL
+        else:
+            listItem = listItem + listUL + '&&&' + listOL
+        pageListItems.append(listItem)
+
+    mainHeadings.append(pageMainHeadings)
+    mainContents.append(pageMainContents)
+    videoHeadings.append(pageVideoHeadings)
+    videoURLs.append(pageVideoURLs)
+    subHeadingsH2.append(pageSubHeadingsH2)
+    subHeadingsH3.append(pageSubHeadingsH3)
+    imageURLs.append(pageImageURLs)
+    facts.append(pageFacts)
+    definitions.append(pageDefinitions)
+    explanations.append(pageExplanations)
+    snippets.append(pageSnippets)
+    codes.append(pageCodes)
+    outputs.append(pageOutputs)
+    tableData.append(pageTableData)
+    listItems.append(pageListItems)
+
+data = {'main_headings': mainHeadings, 'main_contents': mainContents, 'video_headings': videoHeadings,
+        'video_urls': videoURLs, 'sub_headings_h2': subHeadingsH2, 'sub_headings_h3': subHeadingsH3,
+        'image_urls': imageURLs, 'facts': facts, 'definitions': definitions, 'explanations': explanations,
+        'snippets': snippets, 'codes': codes, 'outputs': outputs, 'table_data': tableData, 'list_items': listItems}
+
+df = pd.DataFrame(data)
+df.to_csv('Programiz_Swift.csv')
---
+++ W3Schools/w3school_Kotlin.py
@@ -0,0 +1,155 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+mainURL="https://www.w3schools.com/kotlin/index.php"
+page = requests.get(mainURL)
+htmlContent = page.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+
+urls = []
+pages = soup.find('div', {'id': 'leftmenuinnerinner'}).findAll(href=True)
+for slug in pages:
+    urls.append('https://www.w3schools.com/kotlin/' + slug.get('href'))
+
+mainHeadings = []
+subHeadingsH2 = []
+subHeadingsH3 = []
+links = []
+imageURLs = []
+facts = []
+definitions = []
+explanations = []
+codes = []
+listItems = []
+tableData = []
+data = {}
+
+for url in urls:
+    page = requests.get(url)
+    htmlContent = page.content
+    soup = BeautifulSoup(htmlContent, 'html.parser')
+
+    subContent = soup.find('div', {"id": "main"})  # Page Sub Content
+    if subContent is None:
+        continue
+
+    mainHeading = subContent.findAll('h1')  # Main Heading
+    pageMainHeadings = []
+    if mainHeading is not None:
+        for head in mainHeading:
+            pageMainHeadings.append(head.text)
+
+    subHeadingH2 = subContent.findAll('h2')  # Sub Headings h2
+    pageSubHeadingsH2 = []
+    if subHeadingH2 is not None:
+        for head in subHeadingH2:
+            pageSubHeadingsH2.append(head.text)
+
+    subHeadingH3 = subContent.findAll('h3')  # Sub Headings h3
+    pageSubHeadingsH3 = []
+    if subHeadingH3 is not None:
+        for head in subHeadingH3:
+            pageSubHeadingsH3.append(head.text)
+
+    link = subContent.findAll('a')  # Page Links
+    pageLinks = []
+    if link is not None:
+        for lin in link:
+            if lin.get('href') is not None and not lin.get('href').startswith('http'):
+                pageLinks.append('https://www.w3schools.com/kotlin/' + lin.get('href'))
+            else:
+                pageLinks.append(lin.get('href'))
+
+    images = subContent.findAll('img')  # Images
+    pageImageURLs = []
+    if images is not None:
+        for img in images:
+            pageImageURLs.append('https://www.w3schools.com/kotlin/' + img.get('src'))
+
+    paragraphs = subContent.findAll('p')  # Paragraphs
+    pageFacts = []
+    pageDefinitions = []
+    pageExplanations = []
+    if paragraphs is not None:
+        for para in paragraphs:
+            slug = para.text.replace('\n', '')
+            if 10 <= len(slug) <= 79:
+                pageFacts.append(slug)
+            elif 80 <= len(slug) <= 119:
+                pageDefinitions.append(slug)
+            elif 120 <= len(slug):
+                pageExplanations.append(slug)
+
+    codeContent = subContent.findAll('div', {"class": "w3-code"})  # Codes and Outputs
+    pageCodes = []
+    if codeContent is not None:
+        for slug in codeContent:
+            pageCodes.append(slug.text)
+
+    tables = subContent.findAll('table')  # Tables
+    pageTableData = []
+    if tables is not None:
+        for tb in tables:
+            table = ""
+            rowData = []
+            tableRows = tb.findAll('tr')
+            for row in tableRows:
+                rowData.append(row.text[1:-1].replace('\n', '|||'))
+            for row in rowData:
+                table = table + str(row) + '&&&'
+            pageTableData.append(table[:-3])
+
+    allListUL = subContent.findAll('ul')  # List Items
+    allListOL = subContent.findAll('ol')
+    listUL = ""
+    listOL = ""
+    listItem = ""
+    pageListItems = []
+    if len(allListUL) != 0 or len(allListOL) != 0:
+        if len(allListUL) != 0:
+            for lis in allListUL:
+                listUL = listUL + str(lis.text[1:]).replace('\n', '|||').replace('\t\t', '')
+        if len(allListOL) != 0:
+            for lis in allListOL:
+                listOL = listOL + str(lis.text[1:]).replace('\n', '|||').replace('\t\t', '')
+        if len(allListUL) != 0 and len(allListOL) == 0:
+            listItem = listItem + listUL
+        elif len(allListUL) == 0 and len(allListOL) != 0:
+            listItem = listItem + listOL
+        else:
+            listItem = listItem + listUL + '&&&' + listOL
+        pageListItems.append(listItem)
+
+    # print(pageMainHeadings)
+    # print(pageSubHeadingsH2)
+    # print(pageSubHeadingsH3)
+    # print(pageLinks)
+    # print(pageImageURLs)
+    # print(pageFacts)
+    # print(pageDefinitions)
+    # print(pageExplanations)
+    # print(pageCodes)
+    # print(pageTableData)
+    # print(pageListItems)
+
+    mainHeadings.append(pageMainHeadings)
+    subHeadingsH2.append(pageSubHeadingsH2)
+    subHeadingsH3.append(pageSubHeadingsH3)
+    links.append(pageLinks)
+    imageURLs.append(pageImageURLs)
+    facts.append(pageFacts)
+    definitions.append(pageDefinitions)
+    explanations.append(pageExplanations)
+    codes.append(pageCodes)
+    tableData.append(pageTableData)
+    listItems.append(pageListItems)
+
+
+
+data = {'main_headings': mainHeadings, 'sub_headings_h2': subHeadingsH2, 'sub_headings_h3': subHeadingsH3,
+        'links': links, 'image_urls': imageURLs, 'facts': facts, 'definitions': definitions,
+        'explanations': explanations, 'codes': codes, 'table_data': tableData, 'list_items': listItems}
+
+df = pd.DataFrame(data)
+df.to_csv('W3Schools_Kotlin.csv')
